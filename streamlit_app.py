a8ee22665215cb1e366b9c033135ce220e34b893
Sahil786475 <shah126578@gmail.com>
2026-03-13 01:35:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ import database as db
 import requests
 
 st.set_page_config(
-    page_title="RK RAJA XWD 😈",
+    page_title="SAHIL KHAN XWD 😈",
     page_icon="✅",
     layout="wide",
     initial_sidebar_state="expanded"
